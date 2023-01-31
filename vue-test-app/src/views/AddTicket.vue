a6201501f19f0cd7122a5fd67729ac15340a7eab
yucheng huang <1063215202@qq.com>
2023-01-30 22:22:14 -0800

    差个页面

--- vue-test-app/src/views/AddTicket.vue
+++ vue-test-app/src/views/AddTicket.vue
@@ -1,11 +1,10 @@
 <script setup>
 import goBackArrow from '../store/goBackArrow';
+
 const store = goBackArrow();
 store.goBackArrow();
 
-const value = ref([
-  {url: 'https://fastly.jsdelivr.net/npm/@vant/assets/leaf.jpeg'},
-]);
+const value = ref();
 const result = ref('');
 const showPicker = ref(false);
 const columns = [
@@ -15,55 +14,84 @@ const columns = [
   {text: '问题4', value: 'Shaoxing'},
   {text: '问题5', value: 'Huzhou'},
 ];
-const value1 = ref('');
+const message = ref('');
 
 const onConfirm = ({selectedOptions}) => {
   result.value = selectedOptions[0]?.text;
   showPicker.value = false;
 };
+import axios from "axios";
+
+const route = useRoute();
+const machineId = route.query.id;
+const onSubmit = (values) => {
+  console.log(values.uploader[0].content)
+  var data1 = axios(
+      {
+        url: "/api/ticket/insertTicket",
+        method: "POST",
+        data: {
+          "status": 1,
+          "machineId": machineId,
+          "ticketImg": values.uploader[0].content,
+          "ticketType": values.picker,
+          "ticketDescription": values.message,
+          "ticketStatus": 0
+        },
+        contentType: "json",
+        processData: false,
+        dataType: "json"
+      }
+  ).then(function (response) {
+    // console.log(dataList)
+  });
+};
 </script>
 <template>
-  <van-cell-group inset>
+  <van-form @submit="onSubmit">
+    <van-cell-group inset>
 
-    <van-field name="uploader" label="文件上传">
-      <template #input>
-        <van-uploader v-model="value"/>
-      </template>
-    </van-field>
-    <van-field
-        v-model="result"
-        is-link
-        readonly
-        name="picker"
-        label="选择器"
-        placeholder="选择问题类型"
-        @click="showPicker = true"
-    />
-    <van-popup v-model:show="showPicker" position="bottom">
-      <van-picker
-          :columns="columns"
-          @confirm="onConfirm"
-          @cancel="showPicker = false"
+      <van-field name="uploader" label="文件上传">
+        <template #input>
+          <van-uploader :max-count="1" v-model="value"/>
+        </template>
+      </van-field>
+      <van-field
+          v-model="result"
+          is-link
+          readonly
+          name="picker"
+          label="选择器"
+          placeholder="选择问题类型"
+          @click="showPicker = true"
       />
-    </van-popup>
-    <van-field
-        v-model="message"
-        rows="2"
-        autosize
-        label="留言"
-        type="textarea"
-        maxlength="50"
-        placeholder="请输入留言"
-        show-word-limit
-    />
+      <van-popup v-model:show="showPicker" position="bottom">
+        <van-picker
+            :columns="columns"
+            @confirm="onConfirm"
+            @cancel="showPicker = false"
+        />
+      </van-popup>
+      <van-field
+          name="message"
+          v-model="message"
+          rows="2"
+          autosize
+          label="留言"
+          type="textarea"
+          maxlength="50"
+          placeholder="请输入留言"
+          show-word-limit
+      />
 
 
-    <div style="margin: 16px;">
-      <van-button round block type="primary" native-type="submit">
-        提交
-      </van-button>
-    </div>
-  </van-cell-group>
+      <div style="margin: 16px;">
+        <van-button round block type="primary" native-type="submit">
+          提交
+        </van-button>
+      </div>
+    </van-cell-group>
+  </van-form>
 </template>
 
 
--- vue-test-app/src/views/ListTicket.vue
+++ vue-test-app/src/views/ListTicket.vue
@@ -1,47 +1,113 @@
 <script setup>
 
+
+
+
 const route = useRoute();
-console.log(route.query)
+const machineId = route.query.id;
 
 import goBackArrow from '../store/goBackArrow';
+
 const store = goBackArrow();
 store.goBackArrow();
 
 
-
-const value1 = ref(0);
+const value1 = ref(1);
 const value2 = ref('a');
 const option1 = [
-  {text: '未关闭/open', value: 0},
-  {text: '关闭/close', value: 1},
-  {text: '全部/all', value: 2},
+  {text: '未关闭/open', value: 1},
+  {text: '关闭/close', value: 2},
+  {text: '全部/all', value: 0},
 ];
 const option2 = [
   {text: '默认排序', value: 'a'},
   {text: '最新排序', value: 'b'},
 ];
 
-const list = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+const dataList = ref();
+import axios from "axios";
+
+var data1 = axios(
+    {
+      url: "/api/machines/listTicket",
+      method: "POST",
+      data: {
+        "status": 1,
+        "machineId": machineId
+      },
+      contentType: "json",
+      processData: false,
+      dataType: "json"
+    }
+).then(function (response) {
+  dataList.value = response.data.list;
+  // console.log(dataList)
+});
 const loading = ref(false);
 const finished = ref(false);
+const onLoad = () => {
+  console.log("onload")
+  // loading.value = false;
+  finished.value = true;
+}
+
+const changeStatus = () => {
+  // console.log(value1)
+  axios(
+      {
+        url: "/api/machines/listTicket",
+        method: "POST",
+        data: {
+          "status": value1.value,
+          "machineId": machineId
+        },
+        contentType: "json",
+        processData: false,
+        dataType: "json"
+      }
+  ).then(function (response) {
+    dataList.value = response.data.list;
+    // console.log(pageName)
+  })
+}
+
+const getTicket = (id) => {
+  // this.$router.push({path: 'listTicket', query: {id: id}});
+  // myRouter.push({path: 'listTicket', query: {id: id}});
+  console.log(id)
+  // const router = useRouter();
+  // router.push({path: 'addTicket', query: {id: id}})
+}
+import {useRouter} from "vue-router";
+const router = useRouter();
+const addTicket = () => {
+  // console.log(router)
+  router.push({path: '/addTicket', query: {id: machineId}})
+}
 
 </script>
 <template>
   <van-dropdown-menu>
-    <van-dropdown-item v-model="value1" :options="option1"/>
+    <van-dropdown-item v-model="value1" :options="option1" @change="changeStatus"/>
     <van-dropdown-item v-model="value2" :options="option2"/>
   </van-dropdown-menu>
 
   <van-list
       v-model:loading="loading"
       :finished="finished"
-      finished-text="没有更多了"
       @load="onLoad"
   >
-    <van-cell v-for="item in list" :key="item" :title="item">test</van-cell>
+    <van-cell v-for="item in dataList" :key="item.ticketId" :title="item.ticketType"
+              @click="getTicket(item.ticketId)">
+      <van-tag v-if="item.ticketStatus === 0" type="danger">未关闭</van-tag>
+      <van-tag v-else-if="item.ticketStatus === 1" type="success ">关闭</van-tag>
+    </van-cell>
   </van-list>
 
   <van-back-top bottom="50"/>
+  <van-back-top offset="-1" right="300" bottom="100" @click="addTicket">
+    <van-icon name="plus"/>
+  </van-back-top>
 </template>
 
 
